95e9c33221bb30abad052b4c4341c407fb16490a
Abdelmageed Sayed <abdelmageedsayed@hotmail.com>
2017-03-09 15:43:47 +0200

    Added redux-persist to persist store state on page reload (to preserve authentication status)

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import configureStore from './store/configureStore';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
 import { App } from './components/app';
 import 'bootstrap/dist/css/bootstrap.css';
 
 const store = configureStore();
+persistStore(store);
 const reactRoot = window.document.getElementById("app");
   ReactDOM.render(
     <Provider store={store}>
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,19 +1,20 @@
 import {createStore, compose, applyMiddleware} from 'redux';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import thunk from 'redux-thunk';
+import {autoRehydrate} from 'redux-persist';
 import rootReducer from '../reducers';
 
 function configureStoreProd(initialState) {
   const middlewares = [
     // Add other middleware on this line...
-
     // thunk middleware can also accept an extra argument to be passed to each thunk action
     // https://github.com/gaearon/redux-thunk#injecting-a-custom-argument
     thunk,
   ];
 
   return createStore(rootReducer, initialState, compose(
-    applyMiddleware(...middlewares)
+    applyMiddleware(...middlewares),
+    autoRehydrate()
     )
   );
 }
@@ -32,7 +33,8 @@ function configureStoreDev(initialState) {
 
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // add support for Redux dev tools
   const store = createStore(rootReducer, initialState, composeEnhancers(
-    applyMiddleware(...middlewares)
+    applyMiddleware(...middlewares),
+    autoRehydrate()
     )
   );
 
