4ecd24cb8c8f4696c4573ac444ef6406e4c95c4e
Abdelmageed Sayed <abdelmageedsayed@hotmail.com>
2017-04-30 16:18:09 +0200

    VoteInput component Option name validation.
    cannot be empty.

--- src/components/VoteInput.js
+++ src/components/VoteInput.js
@@ -1,5 +1,5 @@
 import React, {Component, PropTypes} from 'react';
-import {Form, FormGroup, FormControl, Button, HelpBlock} from 'react-bootstrap'; 
+import {Form, FormGroup, InputGroup, FormControl, Button, HelpBlock} from 'react-bootstrap'; 
 
 export default class VoteInput extends Component{
     constructor(props) {
@@ -8,22 +8,45 @@ export default class VoteInput extends Component{
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleChange = this.handleChange.bind(this);
         this.makeNewOption = this.makeNewOption.bind(this);
+        this.getOptionNameValidation = this.getOptionNameValidation.bind(this);
+        this.getStateValidation = this.getStateValidation.bind(this);
 
         this.state = {
-            optionName: ''
+            optionName: '',
+            isValid: true
         };
     }
 
     handleSubmit(e) {
         e.preventDefault();
-        //call addOptiont
+        if(!this.getOptionNameValidation()) {return;}
         const newOption = this.makeNewOption();
         this.props.addNewOption(newOption);
+    }
+
+    getOptionNameValidation() {
+        if (this.state.optionName === '') {
+            this.setState({
+                isValid: false
+            });
+            return false;
+        } else {
+             this.setState({
+                isValid: true
+            });
+            return true;
+        }
+    }
+
+    getStateValidation() {
+        return (this.state.isValid) ? null : "error";
     }
 
     handleChange(e) {
         this.setState({
             optionName: e.target.value 
+        }, () => {
+            this.getOptionNameValidation();
         });
     }
 
@@ -35,19 +58,24 @@ export default class VoteInput extends Component{
     }
 
     render() {
+        const validationState = this.getStateValidation();
         return (
             <Form inline onSubmit={this.handleSubmit}>
-                <FormGroup>
-                    <FormControl 
-                        placeholder="Your own option"
-                        onChange={this.handleChange}
-                        />
-                    <FormControl.Feedback />
-                    <Button
-                        type="submit"
-                    >
-                        Vote
-                    </Button>
+                <FormGroup validationState={validationState}>
+                    <InputGroup>
+                        <FormControl 
+                            placeholder="Your own option"
+                            onChange={this.handleChange}
+                            />
+                        <FormControl.Feedback />
+                        <InputGroup.Button>
+                            <Button type="submit">
+                                Vote
+                            </Button>
+                        </InputGroup.Button>
+                    </InputGroup>
+                    {!this.state.isValid ? 
+                        <HelpBlock>Option name cannot be empty</HelpBlock> : null}
                 </FormGroup>
             </Form>
         );
@@ -55,5 +83,6 @@ export default class VoteInput extends Component{
 }
 
 VoteInput.propTypes = {
-    ip: PropTypes.string
+    ip: PropTypes.string,
+    addNewOption: PropTypes.func
 };
--- src/components/VoteInput.spec.js
+++ src/components/VoteInput.spec.js
@@ -2,23 +2,28 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import {expect} from 'chai';
 import sinon from 'sinon';
-import {FormControl, Button} from 'react-bootstrap';
+import {FormControl, Button, HelpBlock} from 'react-bootstrap';
 
 import VoteInput from './VoteInput';
 
 let wrapper,
-    spyMakeNewOption;
+    sandbox = sinon.sandbox.create(),
+    spyMakeNewOption,
+    spyGetOptionNameValidation,
+    spyGetStateValidation;
 const ip = "123.156.2.1";
 beforeEach(() => {
-    spyMakeNewOption = sinon.spy(VoteInput.prototype, 'makeNewOption');
+    spyMakeNewOption = sandbox.spy(VoteInput.prototype, 'makeNewOption');
+    spyGetOptionNameValidation = sandbox.spy(VoteInput.prototype, 'getOptionNameValidation');
+    spyGetStateValidation = sandbox.spy(VoteInput.prototype, "getStateValidation");
     wrapper = shallow(
     <VoteInput 
         ip={ip}
-        addNewOption={sinon.spy()}/>);
+        addNewOption={sandbox.spy()}/>);
 });
 
 afterEach(() => {
-    spyMakeNewOption.restore();
+    sandbox.restore();
 });
 
 describe('VoteInput', () => {
@@ -41,30 +46,101 @@ describe('VoteInput', () => {
         expect(spyMakeNewOption.returned(option)).to.be.true;
     });
 
-    it('handleSubmit(e) should call props.addNewOption with (newOption) returned from makeNewOption() and prevents form submission', () => {
+    it('getOptionNameValidation() should return (false) if state.optionName is an empty string', () => {
+        wrapper.instance().setState({optionName: ''});
+
+        wrapper.instance().getOptionNameValidation();
+
+        expect(spyGetOptionNameValidation.returned(false)).to.be.true;
+    });
+
+    it('getOptionNameValidation() should return (true) if state.optionName is not an empty string', () => {
+        wrapper.instance().setState({optionName: 'not empty'});
+
+        wrapper.instance().getOptionNameValidation();
+
+        expect(spyGetOptionNameValidation.returned(true)).to.be.true;
+    });
+
+    it('should render an error message (HelpBlock component) on error', () => {
+        wrapper = shallow(<VoteInput />);
+        wrapper.instance().setState({
+            isValid: false
+        });
+
+        const errorMessage = wrapper.find(HelpBlock);
+
+        expect(errorMessage).to.have.length(1);
+    });
+
+    it('handleSubmit(e) should call props.addNewOption with (newOption) returned from makeNewOption() and prevents form submission if input is valid', () => {
         //restore spyMakeNewOption to stub it
         spyMakeNewOption.restore();
+        spyGetOptionNameValidation.restore();
         wrapper.unmount();
-        const stubMakeNewOption = sinon.stub(VoteInput.prototype, 'makeNewOption'),
+        const stubMakeNewOption = sandbox.stub(VoteInput.prototype, 'makeNewOption'),
+            stubGetOptionNameValidation = sandbox.stub(VoteInput.prototype, 'getOptionNameValidation'),
             newOption = {
                 _id: 'newid',
                 votes: ['some.fancy.user.ip']
             };
+
         wrapper = shallow(
          <VoteInput 
             ip={ip}
-            addNewOption={sinon.spy()}/>);
+            addNewOption={sandbox.spy()}/>);
+
         stubMakeNewOption.returns(newOption);
+        stubGetOptionNameValidation.returns(true);
+
         const e = {
-            preventDefault: sinon.spy()
+            preventDefault: sandbox.spy()
         };
 
         wrapper.instance().handleSubmit(e);
 
         expect(e.preventDefault.called).to.be.true;
-        console.log(wrapper.instance().props.addNewOption.args[0]);
         expect(wrapper.instance().props.addNewOption.calledWith(newOption)).to.be.true;
 
         stubMakeNewOption.restore();
+    });
+
+    it('handleSubmit(e) should return after getOptionNameValidation() returns (false)', () => {
+        spyGetOptionNameValidation.restore();
+        wrapper.unmount();
+        const stubGetOptionNameValidation = sandbox.stub(VoteInput.prototype, 'getOptionNameValidation');
+        stubGetOptionNameValidation.returns(false);
+        const e = {
+            preventDefault: sandbox.spy()
+        };
+        wrapper = shallow(
+         <VoteInput 
+            ip={ip}
+            addNewOption={sandbox.spy()}/>);
+
+        wrapper.instance().handleSubmit(e);
+        
+        expect(e.preventDefault.called).to.be.true;
+        expect(wrapper.instance().props.addNewOption.called).to.be.false;
+    });
+
+    it('getStateValidation() should return ("error") if !state.isValid', () => {
+        wrapper.instance().setState({
+            isValid: false
+        });
+        
+        wrapper.instance().getStateValidation();
+
+        expect(spyGetStateValidation.returned("error")).to.be.true;
+    });
+
+    it('getStateValidation() should return (null) if state.isValid', () => {
+        wrapper.instance().setState({
+            isValid: true
+        });
+        
+        wrapper.instance().getStateValidation();
+
+        expect(spyGetStateValidation.returned(null)).to.be.true;
     });
 });
