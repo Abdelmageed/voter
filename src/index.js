39466aa2f57c3b68dda62305c813a672bd7b35f4
Abdelmageed Sayed <abdelmageedsayed@hotmail.com>
2017-05-01 20:18:54 +0200

    initialize session Id on application start.
    to fix first time vistors bypassing client authentication check.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
-// Set up your application entry point here...
-import configureStore from './store/configureStore';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { persistStore } from 'redux-persist';
 import {Router, browserHistory} from 'react-router';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import configureStore from './store/configureStore';
 import {getRoutes} from './routes';
 import {getIp, getAllPolls} from './actions/actionCreators';
-import 'bootstrap/dist/css/bootstrap.css';
+import session from './util/session';
 require("font-awesome-webpack");
 
 const store = configureStore();
 persistStore(store);
 store.dispatch(getIp());
 store.dispatch(getAllPolls());
+if(!store.getState().user.isAuthenticated) {session.setSessionId('');}
 
 const reactRoot = window.document.getElementById("app");
   ReactDOM.render(
