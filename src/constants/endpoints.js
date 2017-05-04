a43159622f8f466efd98f7782c83670d2e072c13
Abdelmageed Sayed <abdelmageedsayed@hotmail.com>
2017-05-04 10:02:39 +0200

    Added base api url for production.

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const apiUrl = 'http://localhost:3000';
+const apiUrl = (process.env.NODE_ENV === 'production') ? 'https://voter-api.herokuapp.com/' : 'http://localhost:3000';
 export const axiosInstance = axios.create({
   baseURL: apiUrl,
   validateStatus: (status)=> {
